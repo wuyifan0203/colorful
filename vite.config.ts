2c1902ccb0ab6ac883f5ea45f7ef4d7e30fbdc04
wuyifan <1208097313@qq.com>
2022-07-21 02:47:47 +0800

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'./'
+  base:'./',
+  build:{
+    outDir:'docs',
+    assetsDir:'assets'
+  }
 })
